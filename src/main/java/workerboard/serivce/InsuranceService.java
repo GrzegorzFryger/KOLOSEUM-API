f0206e1e0782fa2f6e214d0f4b719abac1d47d95
Fryger <s12583@pjwstk.edu.pl>
2019-01-16 23:39:16 +0100

    clean

--- src/main/java/workerboard/serivce/InsuranceService.java
+++ src/main/java/workerboard/serivce/InsuranceService.java
@@ -27,27 +27,25 @@ import java.util.stream.Collectors;
 public class InsuranceService extends BasicAbstractService<InsuranceApplication> {
 
 
-
-
     private InsuranceRepository insuranceRepository;
     private InsuranceHistoryRepository insuranceHistoryRepository;
     private RisksService risksService;
     private ApplicationUserCustomRepository applicationUserRepository;
     private EventProducer eventProducer;
 
-   @Autowired
+    @Autowired
     public InsuranceService(InsuranceRepository insuranceRepository, InsuranceHistoryRepository insuranceHistoryRepository,
                             RisksService risksService, ApplicationUserCustomRepository applicationUserRepository, EventProducer eventProducer) {
-       super.setBasicRepository(insuranceRepository);
+        super.setBasicRepository(insuranceRepository);
         this.insuranceRepository = insuranceRepository;
         this.insuranceHistoryRepository = insuranceHistoryRepository;
         this.risksService = risksService;
         this.applicationUserRepository = applicationUserRepository;
         this.eventProducer = eventProducer;
     }
-    
+
     public InsuranceApplication registerInsuranceApplication(InsuranceApplication insuranceApplication) {
-        if(!isApplicationValid(insuranceApplication)){
+        if (!isApplicationValid(insuranceApplication)) {
             insuranceApplication.addMessage(new ServiceMessage(ServiceMessageType.ERROR, "Tariffication attribute is missing"));
             return insuranceApplication;
         }
@@ -68,11 +66,11 @@ public class InsuranceService extends BasicAbstractService<InsuranceApplication>
     }
 
     private boolean isApplicationValid(InsuranceApplication insuranceApplication) {
-        if(insuranceApplication.getVehicle() == null) return false;
-        if(Integer.valueOf(insuranceApplication.getVehicle().getVehicleValue()) == 0 ||
+        if (insuranceApplication.getVehicle() == null) return false;
+        if (Integer.valueOf(insuranceApplication.getVehicle().getVehicleValue()) == 0 ||
                 insuranceApplication.getVehicle().getVehicleValue() == null) return false;
-        if(insuranceApplication.getPersons().isEmpty() || insuranceApplication.getPersons() == null) return false;
-        if(insuranceApplication.getPersons().get(0).getDayOfBirth() == null) return false;
+        if (insuranceApplication.getPersons().isEmpty() || insuranceApplication.getPersons() == null) return false;
+        if (insuranceApplication.getPersons().get(0).getDayOfBirth() == null) return false;
 
         return insuranceApplication.getPersons().get(0).getDrivingLicenseIssueDate() != null;
 
@@ -81,7 +79,7 @@ public class InsuranceService extends BasicAbstractService<InsuranceApplication>
     public InsuranceApplication getInsuranceApplicationById(Long id) throws NotFound {
 
         Optional<InsuranceApplication> applicationOptional = insuranceRepository.findById(id);
-        if(applicationOptional.isPresent()){
+        if (applicationOptional.isPresent()) {
             InsuranceApplication insuranceApplication = applicationOptional.get();
             insuranceApplication.setRiskVariants(risksService.getAvailableRisks(insuranceApplication));
             return insuranceApplication;
@@ -92,11 +90,11 @@ public class InsuranceService extends BasicAbstractService<InsuranceApplication>
 
     public InsuranceApplication updateInsuranceApplication(Long id, InsuranceApplication insuranceApplication) throws NotFound {
         Optional<InsuranceApplication> optionalApplication = insuranceRepository.findById(id);
-        if(optionalApplication.isPresent()) {
+        if (optionalApplication.isPresent()) {
             InsuranceApplication applicationFromDB = optionalApplication.get();
             applicationFromDB = insuranceApplication;
 
-            if(! (insuranceApplication.getState() == InsuranceApplicationState.CANCELED)) {
+            if (!(insuranceApplication.getState() == InsuranceApplicationState.CANCELED)) {
                 applicationFromDB.setState(InsuranceApplicationState.APPLICATION);
             }
 
@@ -108,14 +106,14 @@ public class InsuranceService extends BasicAbstractService<InsuranceApplication>
 
     public InsuranceApplication acceptInsuranceApplication(Long id, InsuranceApplication insuranceApplication) throws NotFound {
         Optional<InsuranceApplication> optionalApplication = insuranceRepository.findById(id);
-        if(optionalApplication.isPresent()){
+        if (optionalApplication.isPresent()) {
             InsuranceApplication applicationFromDb = optionalApplication.get();
             applicationFromDb = insuranceApplication;
             applicationFromDb.setState(InsuranceApplicationState.POLICY);
             applicationFromDb = insuranceRepository.save(applicationFromDb);
 
             this.eventProducer.createInsuranceNewEvent(applicationFromDb);
-            eventProducer.createNotificationEvent(TypeNotification.POINT_FOR_POLICY,applicationFromDb.getSeller().getId());
+            eventProducer.createNotificationEvent(TypeNotification.POINT_FOR_POLICY, applicationFromDb.getSeller().getId());
             return applicationFromDb;
 
         }
@@ -124,7 +122,7 @@ public class InsuranceService extends BasicAbstractService<InsuranceApplication>
 
     public List<InsuranceApplication> getInsuranceApplicationByUser(Long id) throws NotFound {
         Optional<ApplicationUser> applicationUserOptional = applicationUserRepository.findById(id);
-        if(applicationUserOptional.isPresent()){
+        if (applicationUserOptional.isPresent()) {
             ApplicationUser user = applicationUserOptional.get();
             return insuranceRepository.findAllBySeller(user);
         }
@@ -135,38 +133,38 @@ public class InsuranceService extends BasicAbstractService<InsuranceApplication>
 
     public List<InsuranceApplication> findLike(Map<String, String> attribute, Long id) {
 
-       return
-               super.findAllByCriteria(likeCriteria(), attribute)
-                       .stream()
-                       .filter( x-> x.getSeller().getId() == id)
-                       .collect(Collectors.toList());
+        return
+                super.findAllByCriteria(likeCriteria(), attribute)
+                        .stream()
+                        .filter(x -> x.getSeller().getId() == id)
+                        .collect(Collectors.toList());
 
-   }
-
-
-   public List<ChartData> getChartData(Long id, LocalDate start, LocalDate end ) {
+    }
 
 
-       List<InsuranceApplication> temp = this.insuranceRepository
-               .findAll((Specification<InsuranceApplication>) (root, criteriaQuery, criteriaBuilder) ->
-                       criteriaBuilder.between(root.get("registerDate"), start, end));
+    public List<ChartData> getChartData(Long id, LocalDate start, LocalDate end) {
 
 
-       return temp.stream()
-               .filter(x -> x.getSeller().getId() == id)
-               .collect(Collectors.groupingBy(InsuranceApplication::getRegisterDate))
-               .entrySet()
-               .stream()
-               .map(t -> {
-                   return new ChartData(t.getKey().toString(),
-                           t.getKey(),
-                           t.getValue().stream().mapToInt(c -> c.getTotalPolicyValue()).sum(),
-                           t.getValue().stream().count()
-                   );
-               }).sorted(Comparator
-                       .comparing(data -> data.getDate()))
-               .collect(Collectors.toList());
+        List<InsuranceApplication> temp = this.insuranceRepository
+                .findAll((Specification<InsuranceApplication>) (root, criteriaQuery, criteriaBuilder) ->
+                        criteriaBuilder.between(root.get("registerDate"), start, end));
 
-   }
 
-   }
+        return temp.stream()
+                .filter(x -> x.getSeller().getId() == id)
+                .collect(Collectors.groupingBy(InsuranceApplication::getRegisterDate))
+                .entrySet()
+                .stream()
+                .map(t -> {
+                    return new ChartData(t.getKey().toString(),
+                            t.getKey(),
+                            t.getValue().stream().mapToInt(c -> c.getTotalPolicyValue()).sum(),
+                            t.getValue().stream().count()
+                    );
+                }).sorted(Comparator
+                        .comparing(data -> data.getDate()))
+                .collect(Collectors.toList());
+
+    }
+
+}
